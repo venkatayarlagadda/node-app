dc973672fa9dc894b2911816fe9e067a3e972890
venkatayarlagadda <79134991+venkatayarlagadda@users.noreply.github.com>
2021-02-26 11:50:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+var cors = require("cors");
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('<h1 style="color:red;">Venkat Testing Jode JS with AWS CI/CD Integration</h1> \n');
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send(
+    '<h1 style="color:red;">Testing Jode JS with AWS CI/CD Integration</h1> \n'
+  );
 });
 
 app.listen(PORT, HOST);
